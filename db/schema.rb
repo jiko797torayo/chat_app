7936e4d7ff7ab583f1e940209f1f18ecdd613695
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-11-29 14:28:36 +0900

    devise導入

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181126033445) do
+ActiveRecord::Schema.define(version: 20181129050100) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "contents",   limit: 65535
@@ -22,5 +22,19 @@ ActiveRecord::Schema.define(version: 20181126033445) do
     t.integer  "to",         limit: 4
     t.string   "picture",    limit: 255
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                   limit: 255,              null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
