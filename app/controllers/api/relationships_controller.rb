5b8dfbf2b3f944c7f9e5e242454d61f72c7dca54
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-12-08 10:35:06 +0900

    不要なコードを削除

--- app/controllers/api/relationships_controller.rb
+++ app/controllers/api/relationships_controller.rb
@@ -1,14 +1,5 @@
 module Api
   class RelationshipsController < ApplicationController
-    # def index
-    #   @users = []
-    #   users = Relationship.where(from_user_id: current_user.id)
-    #   users.each do |user|
-    #     @users << User.find(user.to_user_id)
-    #   end
-    #   render json: @users
-    # end
-
     def create
       user = User.find(params[:to_user_id])
       user.relationship(current_user)
