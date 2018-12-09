dea4886437ffaffd8c2abc06ed4949f9d6a6feb4
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-12-09 13:46:52 +0900

    チャットページ初回アクセス時にreplyBoxを表示させないように変更

--- app/assets/javascripts/src/components/messages/replyBox.js
+++ app/assets/javascripts/src/components/messages/replyBox.js
@@ -39,20 +39,26 @@ class ReplyBox extends React.Component {
     })
   }
   render() {
-    return (
-      <div className='reply-box'>
-        <input
-          value={this.state.value}
-          onKeyDown={this.handleKeyDown.bind(this)}
-          onChange={this.updateValue.bind(this)}
-          className='reply-box__input'
-          placeholder='Type message to reply..'
-        />
-        <span className='reply-box__tip'>
-          Press <span className='reply-box__tip__button'>Enter</span> to send
-        </span>
-      </div>
-    )
+    if (typeof this.state.userID === 'number') {
+      return (
+        <div className='reply-box'>
+          <input
+            value={this.state.value}
+            onKeyDown={this.handleKeyDown.bind(this)}
+            onChange={this.updateValue.bind(this)}
+            className='reply-box__input'
+            placeholder='Type message to reply..'
+          />
+          <span className='reply-box__tip'>
+            Press <span className='reply-box__tip__button'>Enter</span> to send
+          </span>
+        </div>
+      )
+    } else {
+      return (
+        <div className='first-notice'></div>
+      )
+    }
   }
 }
 
