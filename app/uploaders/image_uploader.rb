3e4d5a3138c0f8fc6a190ddbce3176b054fc639f
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-12-09 19:55:36 +0900

    画像を送信できる機能を実装

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,8 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  storage :file
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [200, 200]
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end
