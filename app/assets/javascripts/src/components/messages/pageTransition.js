305f847fce5757f140ebc66db2b99cfc4914d1d1
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-12-11 16:37:01 +0900

    ホームボタンとマイページボタンを追加

--- app/assets/javascripts/src/components/messages/pageTransition.js
+++ app/assets/javascripts/src/components/messages/pageTransition.js
@@ -4,9 +4,8 @@ class PageTransition extends React.Component {
   render() {
     return (
       <div className='page-transition'>
-        <div className='page-transition__title'>Chat App</div>
-        <div className='page-transition__logout'>Logout</div>
-        <div className='page-transition__my-page'>My Page</div>
+        <a className='page-transition__title' href='/'>Chat App</a>
+        <a className='page-transition__my-page' href={'/users/' + document.getElementById('currentUser').dataset.id}>My Page</a>
       </div>
     )
   }
