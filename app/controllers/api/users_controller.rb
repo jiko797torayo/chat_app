5be97461552043fbeaed726b4a8019736ed1d00b
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-12-11 21:56:15 +0900

    ユーザーリストの読み込みを高速化

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,12 @@
 module Api
   class UsersController < ApplicationController
     def index
-      @users = []
       users = Relationship.where(from_user_id: current_user.id)
+      to_user_ids = []
       users.each do |user|
-        @users << User.find(user.to_user_id)
+        to_user_ids << user.to_user_id
       end
+      @users = User.where(id: to_user_ids)
       render json: @users
     end
   end
