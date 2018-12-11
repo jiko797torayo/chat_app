1240b09867cdea386e034461550125a7a9544c90
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-12-11 15:11:51 +0900

    ページ遷移ボタンのビュー作成

---
+++ app/assets/javascripts/src/components/messages/pageTransition.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class PageTransition extends React.Component {
+  render() {
+    return (
+      <div className='page-transition'>
+        <div className='page-transition__title'>Chat App</div>
+        <div className='page-transition__logout'>Logout</div>
+        <div className='page-transition__my-page'>My Page</div>
+      </div>
+    )
+  }
+}
+
+export default PageTransition
